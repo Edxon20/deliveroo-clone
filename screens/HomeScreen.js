e91a2d32354627a659594e5e819b58c604be7180
John Doe <edixon20pes@gmail.com>
2023-05-30 12:11:09 -0400

    [ADD] Categories and CategoryCard

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TextInput } from 'react-native'
+import { View, Text, Image, TextInput,ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -8,6 +8,8 @@ import {
     MagnifyingGlassIcon,
     AdjustmentsVerticalIcon,
   } from "react-native-heroicons/outline";
+
+import Categories from "../components/Categories"
 
  
 const HomeScreen = () => {
@@ -23,16 +25,17 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">    
-      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image 
           source={{
             uri: 'https://links.papareact.com/wru',
           }}
           className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         /> 
+        
 
         <View className="flex-1">
-          <Text className="font-bold text-gray text-xs">Deliver  Now!</Text>
+          <Text className="font-bold text-gray text-xs">Deliver Now!</Text>
           <Text className="font-bold text-xl">
             Current Location
             <ChevronDownIcon size={20} color="#00CCBB" />                
@@ -41,7 +44,7 @@ const HomeScreen = () => {
           <UserIcon size={35} color="#00CCBB" />        
       </View> 
           {/* SEARCH */}
-      <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
         <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3"  >
             <MagnifyingGlassIcon size={20}  color="gray" />    
             <TextInput 
@@ -52,7 +55,23 @@ const HomeScreen = () => {
 
         <AdjustmentsVerticalIcon color="#00CCBB"  />
       </View>   
-      
+          
+        {/* BODY */}
+        <ScrollView 
+          className="bg-gray-100"
+          contentContainerStyle={{
+            paddingBottom:100,
+          }}
+        >
+          
+          {/* Components */}
+          {/* Features Rows*/}
+          <Categories />
+          
+
+        </ScrollView>
+
+
   </SafeAreaView>
   )
 }
