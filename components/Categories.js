380545b8d301394b8937afd0e161cd95afffbeb6
John Doe <edixon20pes@gmail.com>
2023-05-30 12:43:30 -0400

    [Finalize] Test Categories

--- components/Categories.js
+++ components/Categories.js
@@ -14,9 +14,33 @@ const Categories = () => {
         >
             {/* Category Card */}
 
-            <CategoryCard imgUrl='https://links.papareact.com/wru' title='testing 1' />
-            <CategoryCard imgUrl='https://links.papareact.com/wru' title='testing 2' />
-            <CategoryCard imgUrl='https://links.papareact.com/wru' title='testing 3' />
+            <CategoryCard
+                imgUrl='https://links.papareact.com/gn7'
+                title='testing 1' />
+
+            <CategoryCard
+                imgUrl='https://links.papareact.com/gn7'
+                title='testing 2' />
+
+            <CategoryCard
+                imgUrl='https://links.papareact.com/gn7'
+                title='testing 3' />
+
+            <CategoryCard
+                imgUrl='https://links.papareact.com/gn7'
+                title='testing 4' />
+
+            <CategoryCard
+                imgUrl='https://links.papareact.com/gn7'
+                title='testing 5' />
+
+            <CategoryCard
+                imgUrl='https://links.papareact.com/gn7'
+                title='testing 6' />
+
+            <CategoryCard
+                imgUrl='https://links.papareact.com/gn7'
+                title='testing 7' />
         </ScrollView>
     )
 }
