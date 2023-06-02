1e24d164b549701566a83f30d403e0289bf15784
John Doe <edixon20pes@gmail.com>
2023-06-02 11:19:25 -0400

    [ADD] dish into Sanity

--- sanity/schemas/category.js
+++ sanity/schemas/category.js
@@ -2,18 +2,26 @@ import {defineField, defineType} from 'sanity'
 
 export default defineType({
   name: 'category',
-  title: 'Menu category',
+  title: 'category',
   type: 'document',
   fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    }),
+   {
+    name: "name",
+    type: "string",
+    title: "Category name",
+    validation: (Rule) => Rule.required(),
+   },
+
+   {
+    name: "image",
+    type: "image",
+    title: "Image of Category",
+    validation: (Rule) => Rule.required(),
+   },
+
+
+    
   ],
+
+ 
 })
