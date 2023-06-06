1a7b2739d08dc9a920a3645cd2ce5e1235073247
John Doe <edixon20pes@gmail.com>
2023-06-06 18:26:47 -0400

    [ADD] navigate into RestaurantCard from App.js

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { StarIcon } from 'react-native-heroicons/solid';
 import { MapPinIcon } from 'react-native-heroicons/outline';
 import { urlFor } from '../sanity';
-
+import { useNavigation } from '@react-navigation/native';
 
 const RestaurantCard = ({
     id,
@@ -17,36 +17,48 @@ const RestaurantCard = ({
     long,
     lat
 }) => {
+
+
+    const navigation = useNavigation();
+
     return (
         <TouchableOpacity
-            className="bg-white mr-3 shadow"
-        >
+            onPress={() => {
+                navigation.navigate('Restaurant', {
+                    id,
+                    imgUrl,
+                    title,
+                    rating,
+                    genre,
+                    address,
+                    short_description,
+                    dishes,
+                    long,
+                    lat
+                });
+            }}
+
+            className="bg-white mr-3 shadow">
             <Image
                 source={{
                     uri: urlFor(imgUrl).url(),
                 }}
                 className='h-36 w-64 rounded-sm'
-                
             />
-            
             <View className="px-3 pb-4 ">
                 <Text className="font-bold text-lg pt-2">{title}</Text>
                 <View className="flex-row items-center space-x-1">
                     <StarIcon size={22} color="green" opacity={0.5} />
                     <Text className="text-xs text-gray-500">
                         <Text className="text-green-500"> {rating} </Text> .{genre}
-
                     </Text>
                 </View>
-
                 <View className="flex-row items-center space-x-1">
-                    <MapPinIcon size={22} color="green" opacity={0.5}/>
+                    <MapPinIcon size={22} color="green" opacity={0.5} />
                     <Text className="text-xs text-gray-500">Nearby . {address}</Text>
                 </View>
-
             </View>
         </TouchableOpacity>
     )
 }
-
 export default RestaurantCard
---
+++ screens/RestaurantScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class RestaurantScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>RestaurantScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default RestaurantScreen
