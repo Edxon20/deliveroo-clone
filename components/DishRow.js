cd290f7761115497cd9191b7779b6ad8d6d94e43
John Doe <edixon20pes@gmail.com>
2023-06-07 14:02:25 -0400

    [ADD] DishRow

---
+++ components/DishRow.js
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DishRow = (props) => {
+  return (
+    <View>
+      <Text>DishRow</Text>
+      
+    </View>
+
+    
+  )
+}
+
+export default DishRow
