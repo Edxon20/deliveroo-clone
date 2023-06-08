d4cffd712e45b9874e8c3ab6d7dab40d75c91dc4
John Doe <edixon20pes@gmail.com>
2023-06-08 13:17:19 -0400

    [ADD] BasketScreen

--- components/basketIcon.js
+++ components/basketIcon.js
@@ -13,7 +13,9 @@ const BasketIcon = () => {
 
   return (
     <View className = "absolute bottom-10 w-full z-50"> 
-      <TouchableOpacity className="bg-[#00CCBB] font-bold mx-5 p-4 rounded-lg 
+      <TouchableOpacity 
+        onPress={() =>navigation.navigate("Basket")}
+        className="bg-[#00CCBB] font-bold mx-5 p-4 rounded-lg 
       flex-row items-center space-x-2">
         <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">{items.length}</Text>
         <Text className="flex-1 font-extrabold text-lg text-center">View Basket</Text>
