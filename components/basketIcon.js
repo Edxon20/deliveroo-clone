593f8c57fdd778900689e091bb5f3a8d1f5e570a
John Doe <edixon20pes@gmail.com>
2023-06-08 13:07:37 -0400

    [Finalize] BasketIcon

--- components/basketIcon.js
+++ components/basketIcon.js
@@ -16,7 +16,7 @@ const BasketIcon = () => {
       <TouchableOpacity className="bg-[#00CCBB] font-bold mx-5 p-4 rounded-lg 
       flex-row items-center space-x-2">
         <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">{items.length}</Text>
-        <Text>View Basket</Text>
+        <Text className="flex-1 font-extrabold text-lg text-center">View Basket</Text>
         <Text className='font-bold text-white'>{basketTotal}$</Text>
       </TouchableOpacity>
     </View>
